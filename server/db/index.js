48777fc66bce8dfa7eff7f3159707c10fc0079f2
speedturtle28 <jackw4@vt.edu>
2022-01-11 16:48:04 +0000

    Added Associations for user and product

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,17 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Product = require('./models/Product')
 
 //associations could go here!
+//They should have a Many-Many relationship
+User.hasMany(Product, { through: 'UserProduct' });
+Product.belongsToMany(User, { through: 'UserProduct' });
 
 module.exports = {
   db,
   models: {
     User,
+    Product,
   },
 }
