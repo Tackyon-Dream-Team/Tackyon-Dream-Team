338b26c09c6cc52286fb32d027d15bad7b83a9b8
Jalilur Rahman <jrahman90.work@gmail.com>
2022-01-13 15:35:59 -0500

    added cart button on all products

--- client/store/Products.js
+++ client/store/Products.js
@@ -1,32 +1,31 @@
 import axios from "axios";
-import { Switch } from "react-router";
 
-const SET_PRODUCTS = 'SET_PRODUCTS' ;
+const SET_PRODUCTS = "SET_PRODUCTS";
 
 const setProducts = (products) => ({
-    type: SET_PRODUCTS,
-    products
+  type: SET_PRODUCTS,
+  products,
 });
 
 export const getProducts = () => {
-    return async (dispatch) => {
-        try {
-            const {data} = await axios.get('/api/products')
-            console.log('get product thunk', data)
-            dispatch(setProducts(data))
-        } catch (error) {
-            console.log('get product thunk something wrong', error)
-        }
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get("/api/products");
+      console.log("get product thunk", data);
+      dispatch(setProducts(data));
+    } catch (error) {
+      console.log("get product thunk something wrong", error);
     }
-}
+  };
+};
 
 const intialState = [];
 
 export default function ProductsReducer(state = intialState, action) {
-    switch(action.type) {
-        case SET_PRODUCTS:
-            return action.products;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case SET_PRODUCTS:
+      return action.products;
+    default:
+      return state;
+  }
 }
