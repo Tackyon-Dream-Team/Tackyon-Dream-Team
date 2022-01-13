8ce550cab3617dc453448bba286ce17d26075fbe
Johnny Vela <vela3733@gmail.com>
2022-01-13 12:21:43 -0800

    refactored allProducts and centered content: all working

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import { getProducts } from '../store/Products'
 
 const AllProducts = (props) => {
      const allProducts = props.products
@@ -8,40 +7,17 @@ const AllProducts = (props) => {
     return (
         <div>
             {allProducts.map(product => 
-                <div key={product.id} className = "product" >
+                <div key={product.id} className = "product" align="center" >
                     <h3>{product.name}</h3>
                     <img src={product.imageUrl} />
-                    <p>{product.price}</p>
+                    <p>{'$' + product.price * .01}</p>
                     <p>{product.description}</p>
                     <br />
                 </div>)}
         </div>
     )
 }
-// class AllProducts extends React.Component {
-    
-//     componentDidMount() {
-//         console.log('component did mount all products');
-//         this.props.loadproducts()
-//     }
 
-//     render() {
-//         console.log('allproducts props', this.props)
-//         return (
-//             <div>
-//                 {allProducts.map(product => 
-//                     <div key={product.id} className = "product" >
-//                         <h3>{product.name}</h3>
-//                         <img src={product.imageUrl} />
-//                         <p>{product.price}</p>
-//                         <p>{product.description}</p>
-//                         <br />
-//                     </div>)}
-//             </div>
-//         )
-//     }
-    
-// }
 
 const mapState = (state) => {
     return {
@@ -49,9 +25,7 @@ const mapState = (state) => {
     }
 };
 
-// const mapDispatch = (dispatch) => {
-//     loadproducts: () => dispatch(getProducts())
-// }
+
  
 
 export default connect(mapState)(AllProducts)
