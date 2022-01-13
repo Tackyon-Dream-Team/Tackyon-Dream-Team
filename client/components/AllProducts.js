7514ac293c38d010a1fe90e263072c4ebe775b6d
Johnny Vela <vela3733@gmail.com>
2022-01-13 11:57:16 -0800

    state is being set with allproducts and allproducts view is created as well as the store

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -2,46 +2,46 @@ import React from 'react'
 import {connect} from 'react-redux'
 import { getProducts } from '../store/Products'
 
-// const AllProducts = (props) => {
-//      const allProducts = props.products
-//      console.log(props)
-//     return (
-//         <div>
-//             {allProducts.map(product => 
-//                 <div key={product.id} className = "product" >
-//                     <h3>{product.name}</h3>
-//                     <img src={product.imageUrl} />
-//                     <p>{product.price}</p>
-//                     <p>{product.description}</p>
-//                     <br />
-//                 </div>)}
-//         </div>
-//     )
-// }
-class AllProducts extends React.Component {
-    
-    componentDidMount() {
-        console.log('component did mount all products')
-        loadproducts()
-    }
-
-    render() {
-        console.log('allproducts props', this.props)
-        return (
-            <div>
-                {allProducts.map(product => 
-                    <div key={product.id} className = "product" >
-                        <h3>{product.name}</h3>
-                        <img src={product.imageUrl} />
-                        <p>{product.price}</p>
-                        <p>{product.description}</p>
-                        <br />
-                    </div>)}
-            </div>
-        )
-    }
-    
+const AllProducts = (props) => {
+     const allProducts = props.products
+     console.log('allproducts-----', props)
+    return (
+        <div>
+            {allProducts.map(product => 
+                <div key={product.id} className = "product" >
+                    <h3>{product.name}</h3>
+                    <img src={product.imageUrl} />
+                    <p>{product.price}</p>
+                    <p>{product.description}</p>
+                    <br />
+                </div>)}
+        </div>
+    )
 }
+// class AllProducts extends React.Component {
+    
+//     componentDidMount() {
+//         console.log('component did mount all products');
+//         this.props.loadproducts()
+//     }
+
+//     render() {
+//         console.log('allproducts props', this.props)
+//         return (
+//             <div>
+//                 {allProducts.map(product => 
+//                     <div key={product.id} className = "product" >
+//                         <h3>{product.name}</h3>
+//                         <img src={product.imageUrl} />
+//                         <p>{product.price}</p>
+//                         <p>{product.description}</p>
+//                         <br />
+//                     </div>)}
+//             </div>
+//         )
+//     }
+    
+// }
 
 const mapState = (state) => {
     return {
@@ -54,4 +54,4 @@ const mapDispatch = (dispatch) => {
 }
  
 
-export default connect(mapState)(AllProducts)
+export default connect(mapState, mapDispatch)(AllProducts)
