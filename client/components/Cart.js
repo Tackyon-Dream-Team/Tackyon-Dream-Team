d76ce5d4acb991faaa50b015a80d1d0cb70fcd3a
Johnny Vela <vela3733@gmail.com>
2022-01-14 19:51:56 -0800

    cartView component and store functioning also displaying cart products

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -23,44 +23,40 @@ class Cart extends React.Component {
   }
 
   render() {
-      return (
-        <div>
-          <h1>Cart View</h1>
-        </div>
-      )
       
-//     const cart = this.props.cart;
-//     if (cart.length === 0) {
-//       return <div>Loading...</div>;
-//     } else {
-//       const products = cart[0].products;
-//       console.log("!!!!!!!!", products);
-//       return (
-//         <div id="single-Order">
-//           {products.map((product) => {
-//             return (
-//               <div key={product.id}>
-//                 <h1>{product.name}</h1>
-//                 <h3>
-//                   ${Math.floor(product.price / 100)}.{product.price % 100}
-//                 </h3>
-//                 <h3>{product.quantity}</h3>
-//                 <h3>About this Item:</h3>
-//                 <p>{product.description}</p>
-//                 <img src={product.imageUrl} className="SinglePicture" />
-//               </div>
-//             );
-//           })}
-//         </div>
-//       );
-//     }
+    const cart = this.props.cart || [];
+    console.log('render cart ' , cart)
+    if (cart.length === 0) {
+      return <div>Cart is Empty</div>;
+    } else {
+      const products = cart[0].products;
+      console.log("!!!!cartItems!!!!", products);
+      return (
+        <div id="single-Order">
+          {products.map((product) => {
+            return (
+              <div key={product.id}>
+                <h1>{product.name}</h1>
+                <h3>
+                  ${Math.floor(product.price / 100)}.{product.price % 100}
+                </h3>
+                <h3>{product.quantity}</h3>
+                <h3>About this Item:</h3>
+                <p>{product.description}</p>
+                <img src={product.imageUrl} className="SinglePicture" />
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
   }
 }
 
 
 const mapState = (state) => {
   return {
-    cart: state.cart,
+    cart: state.Cart,
   };
 };
 
